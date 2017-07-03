d40be6f93781abbe35117127578aab1f97cb350e
Sergey Kapcov <s.kaptsov@smartcodes.ru>
2017-07-03 16:24:29 +0300

    parser

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,9 +5,10 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>parsebills</title>
 </head>
 <body>
-    <h1>test</h1>
+    <h1>Parsebills</h1>
+    <a href="/parse">parse news</a>
 </body>
 </html>
